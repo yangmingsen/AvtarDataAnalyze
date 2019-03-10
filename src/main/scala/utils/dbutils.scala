4b7c4c0cea0b2929a4f85024d989c7729f8e9c45
29DCH <2265940560@qq.com>
2019-03-10 15:04:56 +0800

    统计图表分析ljq

--- src/main/scala/utils/dbutils.scala
+++ src/main/scala/utils/dbutils.scala
@@ -7,17 +7,16 @@ import java.sql.{Connection, DriverManager}
   *
   **/
 object dbutils {
-  def insert(result: String, dbname: String): Unit = {
+  def insert(result: String, sqlstr: String): Unit = {
     val Driver = "com.mysql.jdbc.Driver"
     val url = "jdbc:mysql://rm-uf6871zn4f8aq9vpvro.mysql.rds.aliyuncs.com:3306/job_data?useUnicode=true&characterEncoding=utf8" +
       "&useSSL=false"
     var conn: Connection = null
     var ps: java.sql.PreparedStatement = null
-    val sql = "insert into " + dbname + "(result,jobtype_two_id,column_id) values(?,?,?)"
     try {
       Class.forName(Driver)
       conn = DriverManager.getConnection(url, "user", "Group1234")
-      ps = conn.prepareStatement(sql)
+      ps = conn.prepareStatement(sqlstr)
       ps.setString(1, result)
       ps.setInt(2, 10)
       ps.setInt(3, 14)
