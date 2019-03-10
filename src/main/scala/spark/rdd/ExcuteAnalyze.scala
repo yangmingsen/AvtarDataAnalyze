73de11189f0edc87bba1157a007935b2dd525b3f
29DCH <2265940560@qq.com>
2019-03-10 19:42:21 +0800

    统计图表分析ljq

--- src/main/scala/spark/rdd/ExcuteAnalyze.scala
+++ src/main/scala/spark/rdd/ExcuteAnalyze.scala
@@ -5,7 +5,6 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 import spark.rdd.current._
-import spark.rdd.statistical.EducationCompanyTypeJobNumAnalyze
 import top.ccw.avtar.redis.RedisClient
 
 /** *
@@ -61,10 +60,10 @@ object ExcuteAnalyze {
     val jobsData = dataIn()
 
     //进入时状态分析
-    currentStatus(jobsData,direcion)
+    //currentStatus(jobsData, direcion)
 
     //进入统计图表分析
-    //statisticalGraph(jobsData, direcion)
+    statisticalGraph(jobsData, direcion)
 
   }
 
@@ -85,17 +84,17 @@ object ExcuteAnalyze {
 
     val sqlContext = new SQLContext(sc)
 
-//    val jdbcDF = sqlContext.read.format("jdbc").
-//      options(Map("url" -> "jdbc:mysql://rm-uf6871zn4f8aq9vpvro.mysql.rds.aliyuncs.com/job_data?characterEncoding=utf8&useSSL=false",
-//        "driver" -> "com.mysql.jdbc.Driver", "dbtable" -> "tb_job_info_new", "user" -> "user", "password" -> "Group1234")).load()
-//    jdbcDF.registerTempTable("tb_job_info_new")
-
     val jdbcDF = sqlContext.read.format("jdbc").
-      options(Map("url" -> "jdbc:mysql://127.0.0.1:3306/job_data?characterEncoding=utf8&useSSL=false",
-        "driver" -> "com.mysql.jdbc.Driver", "dbtable" -> "tb_job_info_new", "user" -> "root", "password" -> "ymsyms")).load()
+      options(Map("url" -> "jdbc:mysql://rm-uf6871zn4f8aq9vpvro.mysql.rds.aliyuncs.com/job_data?characterEncoding=utf8&useSSL=false",
+        "driver" -> "com.mysql.jdbc.Driver", "dbtable" -> "tb_job_info_new", "user" -> "user", "password" -> "Group1234")).load()
     jdbcDF.registerTempTable("tb_job_info_new")
 
-    val jobDF = sqlContext.sql("SELECT * FROM `tb_job_info_new` WHERE id BETWEEN 1 AND 3000")
+    /*  val jdbcDF = sqlContext.read.format("jdbc").
+        options(Map("url" -> "jdbc:mysql://127.0.0.1:3306/job_data?characterEncoding=utf8&useSSL=false",
+          "driver" -> "com.mysql.jdbc.Driver", "dbtable" -> "tb_job_info_new", "user" -> "root", "password" -> "ymsyms")).load()
+      jdbcDF.registerTempTable("tb_job_info_new")*/
+
+    val jobDF = sqlContext.sql("SELECT * FROM `tb_job_info_new` WHERE id BETWEEN 1 AND 300")
 
     val rdd1 = jobDF.map(x => {
       val direction = x.getInt(1).toString
@@ -167,7 +166,7 @@ object ExcuteAnalyze {
     //SalaryWorkExperJobNumAveEntityAnalyze.start(jobsData, jobtypeTwoId)
 
     //分析 EducationCompanyTypeJobNum
-    EducationCompanyTypeJobNumAnalyze.start(jobsData, jobtypeTwoId)
+    //EducationCompanyTypeJobNumAnalyze.start(jobsData, jobtypeTwoId)
 
     //分析 EducationJobNumSalaryAve
     //EducationJobNumSalaryAveAnalyze.start(jobsData, jobtypeTwoId)
